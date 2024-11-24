3a046cf4043678411d37d3c4abcf57879799d0ea
Shaswata Roy <63513936+SHASWOTOROY@users.noreply.github.com>
2024-11-24 12:51:01 +0600

    Add files via upload

---
+++ midlabexam/loginvalid.php
@@ -0,0 +1,31 @@
+<?php
+  session_start();
+  if($_SESSION['Lemail']==$_POST['email']){
+    if($_SESSION['Lpassword']==$_POST['password']){
+        header("Location: home.php");
+    }
+    else{
+        header("Location: home.php");
+    }
+  }
+  else{
+    header("Location: login.php");
+  }
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
